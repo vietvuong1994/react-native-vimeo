c7a7a3057651f11a794474de85a0ee7063cfe234
Alex McLeod <alex@mcleod.io>
2016-03-11 11:03:12 -0800

    Added support for onReady, onPlay and onPause event handlers

--- Vimeo.ios.js
+++ Vimeo.ios.js
@@ -16,21 +16,53 @@ import {getPlayerHTML, injectScript} from './webview-content';
 export default class Vimeo extends React.Component {
 
   static propTypes = {
-    videoId: PropTypes.string.isRequired
+    videoId: PropTypes.string.isRequired,
+    onReady: PropTypes.func,
+    onPlay: PropTypes.func,
+    onPlayProgress: PropTypes.func,
+    onPause: PropTypes.func
+  }
+
+  constructor() {
+    super();
+    this.handlers = {};
+    this.state = {
+      ready: false
+    };
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.refs.webviewBridge.sendToBridge('Hi there!');
-    }, 500);
+    // setTimeout(() => {
+    //   this.refs.webviewBridge.sendToBridge('Hi there!');
+    // }, 500);
   }
 
-  getHTMLString() {
-    return getPlayerHTML(this.props.videoId);
+  // api(method, cb) {
+  //   if (!this.state.ready) {
+  //     throw new Error('You cannot use the `api` method until `onReady` has been called');
+  //   }
+  // }
+
+  registerHandlers() {
+    this.registerBridgeEventHandler('ready', this.onReady);
+    this.registerBridgeEventHandler('play', this.props.onPlay);
+    this.registerBridgeEventHandler('playProgress', this.props.onPlayProgress);
+    this.registerBridgeEventHandler('pause', this.props.onPause);
+  }
+
+  onReady() {
+    this.setState({ready: true});
+    if (this.props.onReady) this.props.onReady();
+  }
+
+  registerBridgeEventHandler(eventName, handler) {
+    this.handlers[eventName] = handler;
   }
 
   onBridgeMessage = (message) => {
-    console.log("Received message from webview:", message);
+    let payload = JSON.parse(message);
+    let handler = this.handlers[payload.name];
+    if (handler) handler(payload.data);
   }
 
   render() {
@@ -41,11 +73,11 @@ export default class Vimeo extends React.Component {
           margin: -3,
           height: this.props.height
         }}
-        source={{uri: 'http://localhost:5000/page.html'}}
+        source={{uri: 'http://localhost:5000/?vid=' + this.props.videoId}}
         scalesPageToFit={true}
         scrollEnabled={false}
         onBridgeMessage={this.onBridgeMessage}
-        onError={(error)=>{console.log(error)}}
+        onError={(error)=> console.error(error)}
       />
     );
   }
